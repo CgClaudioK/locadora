ad273276f36bc1b74b6f7312589200ff6b027f1e
CgClaudioK <kosooskiclaudio@gmail.com>
2020-12-02 16:39:50 -0200

    não está salvando CPF ainda

--- src/model/bean/Cliente.java
+++ src/model/bean/Cliente.java
@@ -5,45 +5,48 @@ public class Cliente {
 		private String nome;
 		private int cpf;	
 		private boolean sexo;
-			
+		private String endereco;
 		public String getNome() {
 			return nome;
 		}
 		public void setNome(String nome) {
 			this.nome = nome;
 		}
-		public String setEndereco(String endereco) {
-			return endereco;
-		}
-	
 		public int getCpf() {
 			return cpf;
 		}
 		public void setCpf(int cpf) {
 			this.cpf = cpf;
 		}
-		
 		public boolean isSexo() {
 			return sexo;
 		}
 		public void setSexo(boolean sexo) {
 			this.sexo = sexo;
 		}
-		public void setMasculino(boolean b) {
-			// TODO Auto-generated method stub
-			
-		}
-		public void setFeminino(boolean b) {
-			// TODO Auto-generated method stub
-			
-		}
 		public String getEndereco() {
-			// TODO Auto-generated method stub
-			return null;
+			return endereco;
 		}
+		public void setEndereco(String endereco) {
+			this.endereco = endereco;
+		}
+		public void setMasculino(boolean sexo) {
+			this.sexo = false;
+			
+			
+		}
+		public void setFeminino(boolean sexo) {
+			this.sexo = true;
+			
+		}
+	
+			
+		}
+		
+	
 		
 		
 		
 		
 
-}
+
